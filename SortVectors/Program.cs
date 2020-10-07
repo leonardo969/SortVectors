f6598f667ea622721cc2364890152f11fe28b86a
Leonardo Vincenzi <LEONARDO.VINCENZI@local.ittcesena.it>
2020-10-07 11:07:15 +0200

    first step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] vector = new int [10] { 4,5,8,123,54,-2,1,99,-99,10};
+            for(int i=0; i < 10; i++)
+            {
+                Console.Write(vector[i]+" | ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
